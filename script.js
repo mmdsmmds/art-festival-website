0de5572c376bc638e6e338f5bb6a608c8bddf79a
mmdsmmds <128498677+mmdsmmds@users.noreply.github.com>
2024-11-20 21:43:38 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Replace with your actual Supabase Project URL and Anon Key
+const supabase = supabase.createClient(
+    'https://cqkwcvjxiqqnohjptltu.supabase.co', // Supabase URL
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxa3djdmp4aXFxbm9oanB0bHR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIxMjQzNzYsImV4cCI6MjA0NzcwMDM3Nn0.Y7ceexogbvVz4N0wNe0Z3l6qyVn_HgV1_6tfL-_ceVo' // Supabase Public API Key
+);
+
 document.querySelectorAll('.rate-form').forEach(form => {
     form.addEventListener('submit', async (e) => {
         e.preventDefault(); // Prevent default form submission
